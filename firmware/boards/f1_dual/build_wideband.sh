28fa2d1922b824ae37796ff85f00a8e9bd7eb8ed
Andrey Gusakov <dron0gus@gmail.com>
2022-06-19 18:04:25 +0300

    Revert O0 optimization for all boards, set O0 level for GD boards only

--- firmware/boards/f1_dual/build_wideband.sh
+++ firmware/boards/f1_dual/build_wideband.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+
 #BOARD=f1_dual USE_OPENBLT=yes ../common_make.sh
-BOARD=f1_dual ../common_make.sh
+# set optimization level to 0 until ADC issue is fixed for GD32
+BOARD=f1_dual \
+USE_OPT="-O0 -ggdb -fomit-frame-pointer -falign-functions=16 -fsingle-precision-constant" \../common_make.sh
--- firmware/boards/f1_rev2/build_wideband.sh
+++ firmware/boards/f1_rev2/build_wideband.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-BOARD=f1_rev2 USE_OPENBLT=yes ../common_make.sh
+# set optimization level to 0 until ADC issue is fixed for GD32
+BOARD=f1_rev2 USE_OPENBLT=yes \
+USE_OPT="-O0 -ggdb -fomit-frame-pointer -falign-functions=16 -fsingle-precision-constant" \../common_make.sh
